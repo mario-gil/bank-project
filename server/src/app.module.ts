cf689094ed5b0ad558c6186fd9c246ccaae6550d
Mario Gil Guerrero <mgilguer@emeal.nttdata.com>
2026-01-20 17:41:47 +0100

    postgres

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,28 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TransactionsModule } from './transactions/transactions.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://jsamper92_db_user:m1rwH8iBIety47qv@test.bbbmufj.mongodb.net/banca_db?retryWrites=true&w=majority&appName=test',
-    ),
-    TransactionsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_DATABASE || 'Civica012026',
+      database: process.env.DB_DATABASE || 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV === 'development',
+      logging: true,
+    }),
+    // TransactionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
